171cc6a84e8b2355202aeb12e76c665fe68de475
John Ng <39193864+SunSOG@users.noreply.github.com>
2020-01-27 17:18:00 +0800

    Fixed check.js

--- check.js
+++ check.js
@@ -1,22 +1,25 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 
+async function check(){
 fs.readdir("./kit/beys/", (err, files) => {
   if (err) console.log(err);
   let jsfile = files.filter(f => f.split(".").pop() === "js" && f !== "template.js");
   if (jsfile.length <= 0) {
-    console.log("No Bey found. Please make a Bey first.");
-    return;
+    throw "No Bey found. Please make a Bey first."
   }
 
   jsfile.forEach((f, i) => {
     let props = require(`./kit/beys/${f}`);
-    let bey = new prop("123", 1);
-    if(!bey.name) return console.log(`${f} incomplete. No name found.`);
-    if(!bey.type) return console.log(`${f} incomplete. No type found.`);
-    if(!bey.move) return console.log(`${f} incomplete. No special move name found.`);
-    if(!bey.image) return console.log(`${f} incomplete. No image found.`);
+    let bey = new props("123", 1);
+    if(!bey.name) throw `${f} incomplete. No name found.`;
+    if(!bey.type) throw `${f} incomplete. No type found.`;
+    if(!bey.move) throw `${f} incomplete. No special move name found.`;
+    if(!bey.image) throw `${f} incomplete. No image found.`;
     console.log(`${f} checked!`);
   });
 });
+}
+
+check();
 console.log(`All Beys checked and are ready to be submitted.`);
