2d47811a3bab8ba62e2ea1e91143d47512612f5d
John Ng <39193864+SunSOG@users.noreply.github.com>
2020-05-10 08:00:10 +0800

    Update Beyblade.js

--- kit/class/Beyblade.js
+++ kit/class/Beyblade.js
@@ -61,7 +61,7 @@ class Beyblade {
     .addField("ID", this.id)
     .addField("Original Blader ID",  this.firstOwner);
     
-    message.channel.createMessage(message.channel.id, {embed: embed});
+    message.channel.createMessage({embed: embed});
   }
 }
 
