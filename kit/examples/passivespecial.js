d0518753aeb825dc2c9fc83454158b16220b7679
John Ng <39193864+SunSOG@users.noreply.github.com>
2020-05-20 14:26:32 +0800

    Create passivespecial.js

---
+++ kit/examples/passivespecial.js
@@ -0,0 +1,37 @@
+//Code of the currently most complex Beycord Bey in existance: Demon Destroying Bey! The custom exclusive Bey for a kind of moderators in Beycord's server.
+
+const Discord = require("discord.js");
+const Eris = require("eris");
+const Beyblade = require("../class/Beyblade.js");
+
+class datbey extends Beyblade {
+  constructor(firstOwner, id){
+    super("dat bey", "Balance", "https://randomlinkhereXD", "Passive: Belt Swing\nManual: Spank", firstOwner, id);
+  }
+  special(acted, victim, message, player){
+    super.special(acted, victim, message, player);
+    acted.hp = acted.hp - 60;
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`[${acted.username}] dat bey used **Spank**. The opponent got spanked hard and cried. 60 damage dealt.`)
+    .setColor("#551a8b");
+    message.channel.createMessage({embed:embed});
+  }
+  check(acted, victim, message, player){
+    let bool;
+    if(acted.hp < 60) bool = true;
+    else bool = false;
+    return bool;
+  }
+  passed(acted, victim, message, player){
+    acted.hp = acted.hp - 56;
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`[${acted.username}] dat bey used **Belt Swing**. That hit hard and dealt 56 damage... wow.`)
+    .setColor("#551a8b");
+    message.channel.createMessage({embed:embed});
+  }
+  displayInfo(message){
+    super.displayInfo(message);
+  }
+}
+
+module.exports = datbey;
