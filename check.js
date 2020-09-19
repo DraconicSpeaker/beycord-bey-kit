b84084f9fc01d8d1511c8a19f546907b39feb41f
John Ng <39193864+SunSOG@users.noreply.github.com>
2020-09-19 11:28:05 +0800

    Update check.js

--- check.js
+++ check.js
@@ -80,7 +80,8 @@ fs.readdir("./kit/beys/", (err, files) => {
   jsfile.forEach((f, i) => {
     let props = require(`./kit/beys/${f}`);
     let bey = new props("123", 1);
-    let passed = bey.check(player, dummy, biomessage, {}) || false;
+    let passed = false;
+    if(bey.check) passed = bey.check(player, dummy, biomessage, {}) || false;
     if(passed === true) try{bey.passed(player, dummy, biomessage, {})}catch(err){throw err}
     try{bey.special(player, dummy, biomessage, {})}catch(err){throw err}
     console.log(`${f} passed!`);
